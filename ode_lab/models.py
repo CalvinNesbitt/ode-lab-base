c04a76044036ce3c7de141065320352997acc406
Calvin Nesbitt <cf.nesbitt94@gmail.com>
2024-07-27 16:15:13 +0100

    feat: added some example models

---
+++ ode_lab/models.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def linear_ode(
+    state: np.ndarray, A: np.ndarray = np.array([[-1.0, 0.0], [0.0, -2.0]])
+) -> np.ndarray:
+    return A @ state
+
+
+def l63(
+    state: np.ndarray, sigma: float = 10.0, rho: float = 28.0, beta: float = 8 / 3
+) -> np.ndarray:
+    x, y, z = state
+    dxdt = sigma * (y - x)
+    dydt = x * (rho - z) - y
+    dzdt = x * y - beta * z
+    return np.array([dxdt, dydt, dzdt])
